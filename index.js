d9a0b048108adc11bf372fecbdd3a41941830815
Osborne647 <osborne647@gmail.com>
2026-06-20 06:05:59 -0500

    initial index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_nmt0ayn",
+      "template_qn46w2i",
+      event.target,
+      "fuFqHm5g0Wi-p1K6a",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at omnicodesolution@gmail.com",
+      );
+    });
+}
